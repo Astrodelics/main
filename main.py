dc12f49baa0395535732548316cd8a4de15bd9bc
Astrodelics <mikecurran0000@gmail.com>
2022-09-19 17:57:39 -0400

    Second Class Project

---
+++ main.py
@@ -0,0 +1,10 @@
+phrase1 = "Hello my name is Michael. "
+phrase2 = "I really enjoy computers. "
+phrase3 = "That is why I want to major in computer science "
+print(phrase1 + phrase2 + phrase3)
+
+x = 435
+y = 398
+b = -982
+
+print(x + y * b)
